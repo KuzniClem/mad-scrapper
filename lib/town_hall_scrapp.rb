2f76f8c3e7d192d15b64069e6ddbf73ad281332c
Mcrespo42 <maxime.crespopro@gmail.com>
2019-01-17 14:52:20 +0100

    pour floflo et jules

--- lib/town_hall_scrapp.rb
+++ lib/town_hall_scrapp.rb
@@ -1,3 +1,20 @@
+require 'open-uri'
+require 'nokogiri'
+require 'PRY'
+
 def get_townhall_email(townhall_url)
-	
+	Nokogiri::HTML(open("#{townhall_url}")).xpath('//tbody[1]//tr[4]/td[2]/text()').first.to_s
 end
+
+def get_townhall_url
+  page = Nokogiri::HTML(open("http://www.annuaire-des-mairies.com/val-d-oise.html"))
+  url = page.xpath('/html/body/table//tr[3]//tr[1]//td[2]//table[1]//tr[2]//tr//a/@href').map{|url| url.to_s}
+  binding.pry
+end
+
+get_townhall_url
+
+def perform
+	get_townhall_email()
+end
+
