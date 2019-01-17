eb8d62a99422fc675a846ac92f9bc893897ccc5e
Mcrespo42 <maxime.crespopro@gmail.com>
2019-01-17 18:05:31 +0100

    add spec for crypto + deputy

---
+++ spec/deputy_scrapper_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/deputy_scrapper'
+
+describe "The representative contact method" do
+  it "should return an array" do
+      x = get_deputy_email
+     expect(x).to be_instance_of Array
+  end
+  it "should have an array with 577 elements" do
+      x = get_deputy_email
+     expect(x).to have_attributes(length: 577)
+  end
+end
