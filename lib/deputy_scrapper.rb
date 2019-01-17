5e3911f6d6390a1486d9a9f6e284ab3aec72cdc6
Mcrespo42 <maxime.crespopro@gmail.com>
2019-01-17 18:38:28 +0100

    add downcase on deputy

--- lib/deputy_scrapper.rb
+++ lib/deputy_scrapper.rb
@@ -6,8 +6,9 @@ def get_deputy_email
 	page = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/alphabetique"))
 	deputy_list_a = page.xpath('//*[@id="deputes-list"]//li/a/text()')
 	deputy_names_a = deputy_list_a.map { |single_deputy_information| single_deputy_information.to_s.split.drop(1) }
+	binding.pry
 	deputy_contact_a = deputy_names_a.map { |first_name, last_name|
-		email = first_name + '.' + last_name + '@assemblee-nationale.fr'
+		email = first_name.downcase + '.' + last_name.downcase + '@assemblee-nationale.fr'
 		Hash[first_name: first_name, last_name: last_name, email: email]
 	}
 	# binding.pry
@@ -17,4 +18,4 @@ def perform
 	get_deputy_email
 end
 
-perform
+puts perform
