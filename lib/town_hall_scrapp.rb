fad062661894f08306651405643da3d841eb0ecb
KuzniClem <clement@kuzniewycz.fr>
2019-01-17 19:51:28 +0100

    .

--- lib/town_hall_scrapp.rb
+++ lib/town_hall_scrapp.rb
@@ -9,17 +9,17 @@ end
 def get_townhall_url
   page_url = "http://www.annuaire-des-mairies.com/val-d-oise.html"
   page = Nokogiri::HTML(open("#{page_url}"))
-  url = page.xpath('/html/body/table//tr[3]//tr[1]//td[2]//table[1]//tr[2]//tr//a/@href').map{|url| url.to_s}
-  url.map { |url|
-  	Hash[page.xpath('/html/body/table//tr[3]//tr[1]//td[2]//table[1]//tr[2]//tr//a/text()').map { |town_name|
-  		town_name.to_s
-  	}, get_townhall_email(page_url.gsub(/val-d-oise\.html/, url.gsub(/\A\./)))]
-  }
-  # binding.pry
+  half_url_a = page.xpath('/html/body/table//tr[3]//tr[1]//td[2]//table[1]//tr[2]//tr//a/@href').map { |tmp_url| tmp_url.to_s }
+  binding.pry
+  complete_url_a = half_url_a.map { |tmp_url| "http://www.annuaire-des-mairies.com" +  }
+  list_of_emails = complete_url_a.map { |tmp_url| get_townhall_email(tmp_url) }
+  towns_a = page.xpath('/html/body/table//tr[3]//tr[1]//td[2]//table[1]//tr[2]//tr//a/text()').map { |town_name| town_name.to_s }
+  town_contact_hash_a = towns_a.zip(list_of_emails).map { |town_name, tmp_url| Hash["#{town_name}": tmp_url] }
+  binding.pry
 end
 
 def perform
-	get_townhall_url
+	print get_townhall_url
 end
 
 perform
